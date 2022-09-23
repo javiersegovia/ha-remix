299f5940796d90db91c9e608a29ec2df820d6aa0
Javier Segovia <jsegoviadev@gmail.com>
2022-09-23 14:34:14 -0400

    Add insecure cookies

--- app/session.server.ts
+++ app/session.server.ts
@@ -15,7 +15,7 @@ export const sessionStorage = createCookieSessionStorage({
     path: '/',
     sameSite: 'lax',
     secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === 'production',
+    // secure: process.env.NODE_ENV === 'production', todo: enable when out of beta
   },
 })
 
