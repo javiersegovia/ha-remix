e06357188fb63db8ca6c7a24ca8fffe63ef5a9cb
Javier Segovia <jsegoviadev@gmail.com>
2022-10-07 01:27:22 -0400

    Update pm2 config path

--- pm2.config.js
+++ pm2.config.js
@@ -19,7 +19,7 @@ module.exports = {
       host: "ec2-3-225-58-178.compute-1.amazonaws.com",
       ref: "origin/master",
       repo: "git@github.com:javiersegovia/ha-remix.git",
-      path: "/var/www/remix",
+      path: "/var/www/remix_app",
       "post-deploy": "npm install && npm run generate:schema && npm run build && npm run migrate:deploy && pm2 restart remix",
     },
   },
