fa51f6b4e55c04862a44f1f3023c7a0ee3965bfd
Javier Segovia <javier.segovia@hoytrabajas.com>
2023-01-11 14:17:18 -0400

    Replace gtag script with google tag manager

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,6 @@ import {
   Scripts,
   ScrollRestoration,
   useCatch,
-  useLoaderData,
   useLocation,
   useTransition,
 } from '@remix-run/react'
@@ -24,7 +23,6 @@ import {
 import NProgress from 'nprogress'
 import nProgressStyles from 'nprogress/nprogress.css'
 
-import * as gtag from '~/utils/gtag.client'
 import { getUser } from './session.server'
 import tailwindStylesheetUrl from './styles/tailwind.css'
 import ErrorContainer from './containers/ErrorContainer'
@@ -60,18 +58,13 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 })
 
-const isLocalDev =
-  process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-
 type LoaderData = {
   user: Awaited<ReturnType<typeof getUser>>
-  gaTrackingId: string | undefined
 }
 
 export async function loader({ request }: LoaderArgs) {
-  return json({
+  return json<LoaderData>({
     user: await getUser(request),
-    gaTrackingId: process.env.GA_TRACKING_ID,
   })
 }
 
@@ -79,13 +72,6 @@ export default function App() {
   const location = useLocation()
   const transition = useTransition()
   const { refresh } = useDataRefresh()
-  const { gaTrackingId } = useLoaderData<LoaderData>()
-
-  useEffect(() => {
-    if (!isLocalDev && gaTrackingId?.length) {
-      gtag.pageview(location.pathname, gaTrackingId)
-    }
-  }, [location, gaTrackingId])
 
   useEffect(() => {
     if (
@@ -148,31 +134,27 @@ export default function App() {
             }}
           />
         )}
+
+        <script
+          async
+          id="tagManager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5SRTQ7B');`,
+          }}
+        />
       </head>
 
       <body className="h-full">
-        {isLocalDev || !gaTrackingId ? null : (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-            />
-            <script
-              async
-              id="gtag-init"
-              dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaTrackingId}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-              }}
-            />
-          </>
-        )}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SRTQ7B"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
 
         <Outlet />
         <ScrollRestoration />
