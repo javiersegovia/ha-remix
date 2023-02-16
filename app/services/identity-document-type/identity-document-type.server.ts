e021d977e3c34895f85e3da4f35f40e9df36e3e8
Daniel Capera <caperajn@gmail.com>
2023-02-16 11:02:13 -0500

    add backend by identity document type

--- app/services/identity-document-type/identity-document-type.server.ts
+++ app/services/identity-document-type/identity-document-type.server.ts
@@ -1,3 +1,7 @@
+import type { IdentityDocumentType } from '@prisma/client'
+import type { IdentityDocumentTypeInputSchema } from './identity-document-type.schema'
+
+import { badRequest } from 'remix-utils'
 import { prisma } from '~/db.server'
 
 export const getIdentityDocumentTypes = () => {
@@ -7,4 +11,63 @@ export const getIdentityDocumentTypes = () => {
       name: true,
     },
   })
+}
+
+export const getIdentityDocumentTypeById = async (
+  id: IdentityDocumentType['id']
+) => {
+  return prisma.identityDocumentType.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  })
+}
+
+export const createIdentityDocumentType = async (
+  data: IdentityDocumentTypeInputSchema
+) => {
+  const { name } = data
+  return prisma.identityDocumentType.create({
+    data: { name },
+  })
+}
+
+export const updateIdentityDocumentTypeById = async (
+  id: IdentityDocumentType['id'],
+  data: IdentityDocumentTypeInputSchema
+) => {
+  const { name } = data
+  try {
+    return prisma.identityDocumentType.update({
+      where: {
+        id,
+      },
+      data: { name },
+    })
+  } catch (e) {
+    console.error(e)
+    throw badRequest('No se encontró el ID del tipo de documento de identidad')
+  }
+}
+
+export const deleteIdentityDocumentTypeById = async (
+  id: IdentityDocumentType['id']
+) => {
+  try {
+    const deletedIdentityDocumentType =
+      await prisma.identityDocumentType.delete({
+        where: {
+          id,
+        },
+      })
+
+    return deletedIdentityDocumentType.id
+  } catch (e) {
+    console.error(e)
+    throw badRequest('No se encontró el ID del tipo de documento de identidad')
+  }
 }
