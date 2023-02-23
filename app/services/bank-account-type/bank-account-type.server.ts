e9cbb6969c3f53d41e596d3db10df0cddf80b650
Daniel Capera <caperajn@gmail.com>
2023-02-23 15:01:07 -0500

    add frontend by bank account types

--- app/services/bank-account-type/bank-account-type.server.ts
+++ app/services/bank-account-type/bank-account-type.server.ts
@@ -1,3 +1,8 @@
+import type { BankAccountType } from '@prisma/client'
+import type { BankAccountTypeInputSchema } from './bank-account-type.schema'
+
+import { validationError } from 'remix-validated-form'
+import { badRequest } from 'remix-utils'
 import { prisma } from '~/db.server'
 
 export const getBankAccountTypes = () => {
@@ -7,4 +12,58 @@ export const getBankAccountTypes = () => {
       name: true,
     },
   })
+}
+
+export const getBankAccountTypeById = async (id: BankAccountType['id']) => {
+  return prisma.bankAccountType.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  })
+}
+
+export const createBankAccountType = async (
+  data: BankAccountTypeInputSchema
+) => {
+  const { name } = data
+  return prisma.bankAccountType.create({
+    data: { name },
+  })
+}
+
+export const updateBankAccountTypeById = async (
+  id: BankAccountType['id'],
+  data: BankAccountTypeInputSchema
+) => {
+  const { name } = data
+  try {
+    return prisma.bankAccountType.update({
+      where: {
+        id,
+      },
+      data: { name },
+    })
+  } catch (e) {
+    console.error(e)
+    throw badRequest('No se encontró el ID del tipo de cuenta bancaria')
+  }
+}
+
+export const deleteBankAccountTypeById = async (id: BankAccountType['id']) => {
+  try {
+    const deletedBankAccountType = await prisma.bankAccountType.delete({
+      where: {
+        id,
+      },
+    })
+
+    return deletedBankAccountType.id
+  } catch (e) {
+    console.error(e)
+    throw badRequest('No se encontró el ID del tipo de cuenta bancaria')
+  }
 }
--- app/services/bank-account-type/bank-account-type.test.ts
+++ app/services/bank-account-type/bank-account-type.test.ts
@@ -14,3 +14,70 @@ describe('getBankAccountTypes', () => {
     expect(result).toEqual(bankAccountTypes)
   })
 })
+
+describe('getBankAccountTypeById', () => {
+  it('returns a bank account type', async () => {
+    const expectedBankAccountType = BankAccountTypeFactory.build()
+    vi.spyOn(prisma.bankAccountType, 'findUnique').mockResolvedValueOnce(
+      expectedBankAccountType
+    )
+
+    const result = await bankAccountTypeService.getBankAccountTypeById(
+      expectedBankAccountType.id
+    )
+
+    expect(result).toEqual(expectedBankAccountType)
+  })
+})
+
+describe('createBankAccountType', () => {
+  it('creates and returns a Bank', async () => {
+    const expectedBankAccountType = BankAccountTypeFactory.build()
+
+    vi.spyOn(prisma.bankAccountType, 'create').mockResolvedValueOnce(
+      expectedBankAccountType
+    )
+
+    const result = await bankAccountTypeService.createBankAccountType({
+      name: expectedBankAccountType.name,
+    })
+
+    expect(result).toEqual(expectedBankAccountType)
+  })
+})
+
+describe('updateBankById', () => {
+  it('updates and returns a Bank', async () => {
+    const existingBankAccountType = BankAccountTypeFactory.build()
+    const newBankAccountType = BankAccountTypeFactory.build()
+
+    vi.spyOn(prisma.bankAccountType, 'update').mockResolvedValueOnce(
+      newBankAccountType
+    )
+
+    const result = await bankAccountTypeService.updateBankAccountTypeById(
+      existingBankAccountType.id,
+      {
+        name: newBankAccountType.name,
+      }
+    )
+
+    expect(result).toEqual(newBankAccountType)
+  })
+})
+
+describe('deleteBankAccountTypeById', () => {
+  it('deletes a bank and returns the id', async () => {
+    const existingBankAccountType = BankAccountTypeFactory.build()
+
+    vi.spyOn(prisma.bankAccountType, 'delete').mockResolvedValueOnce(
+      existingBankAccountType
+    )
+
+    const result = await bankAccountTypeService.deleteBankAccountTypeById(
+      existingBankAccountType.id
+    )
+
+    expect(result).toEqual(existingBankAccountType.id)
+  })
+})
