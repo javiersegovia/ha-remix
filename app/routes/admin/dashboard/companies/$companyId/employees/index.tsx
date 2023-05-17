30535aeb609395d856249fe64d4842126d264258
Daniel Capera <caperajn@gmail.com>
2023-05-17 17:12:07 -0500

    create download file csv

--- app/routes/admin/dashboard/companies/$companyId/employees/index.tsx
+++ app/routes/admin/dashboard/companies/$companyId/employees/index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction, LoaderArgs } from '@remix-run/server-runtime'
 
+import { Link } from '@remix-run/react'
 import { json } from '@remix-run/server-runtime'
 import { useLoaderData } from '@remix-run/react'
 import {
@@ -12,7 +13,6 @@ import { getEmployeesByCompanyId } from '~/services/employee/employee.server'
 import { requireAdminUserId } from '~/session.server'
 import { EmployeeList } from '~/components/Lists/EmployeeList'
 import { TitleWithActions } from '~/components/Layout/TitleWithActions'
-import { useEffect } from 'react'
 
 export const loader = async ({ request, params }: LoaderArgs) => {
   await requireAdminUserId(request)
@@ -46,37 +46,7 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 }
 
 export default function AdminDashboardCompanyEmployees() {
-  const { employees, companyName } = useLoaderData<typeof loader>()
-
-  const downloadEmptyCSV = () => {
-    if (employees && employees.length > 0) {
-      const csvContent =
-        'data:text/csv;charset=utf-8,' + encodeURIComponent(getCsvContent())
-      const link = document.createElement('a')
-      link.setAttribute('href', csvContent)
-      link.setAttribute('download', `colaboradores_${company?.id}.csv`)
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    }
-  }
-
-  const getCsvContent = () => {
-    const headers = ['nombre', 'correo_electronico' /* otros campos */].join(
-      ','
-    )
-    const rows = employees.map((employee) => {
-      const { firstName, email /* otros campos */ } = employee
-      return [nombre, correo_electronico /* otros campos */].join(',')
-    })
-    return [headers, ...rows].join('\n')
-  }
-
-  useEffect(() => {
-    if (employees && employees.length > 0) {
-      setCompany(employees[0].company)
-    }
-  }, [employees])
+  const { employees } = useLoaderData<typeof loader>()
 
   return (
     <>
@@ -89,28 +59,27 @@ export default function AdminDashboardCompanyEmployees() {
               <Button
                 className="flex w-full items-center whitespace-nowrap sm:w-auto"
                 href="create"
-                key="create-employee"
                 size="SM"
                 icon={ButtonIconVariants.CREATE}
               >
                 {/* <HiPlus className="mr-3" /> */}
                 Nuevo colaborador
               </Button>
-              <Button
-                className="flex w-full items-center whitespace-nowrap sm:w-auto"
-                onClick={downloadEmptyCSV}
-                key="download-employee"
-                size="SM"
-                variant={ButtonColorVariants.SECONDARY}
-                icon={ButtonIconVariants.DOWNLOAD}
-              >
-                {/* <MdOutlineDownload className="mr-3" /> */}
-                Descargar colaboradores
-              </Button>
+
+              <Link to="download" reloadDocument>
+                <Button
+                  className="flex w-full items-center whitespace-nowrap sm:w-auto"
+                  size="SM"
+                  variant={ButtonColorVariants.SECONDARY}
+                  icon={ButtonIconVariants.DOWNLOAD}
+                >
+                  {/* <MdOutlineDownload className="mr-3" /> */}
+                  Descargar colaboradores
+                </Button>
+              </Link>
 
               <Button
                 className="flex w-full items-center whitespace-nowrap sm:w-auto"
-                href="upload"
                 size="SM"
                 variant={ButtonColorVariants.SECONDARY}
                 icon={ButtonIconVariants.UPLOAD}
