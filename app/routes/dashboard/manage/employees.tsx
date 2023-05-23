631f1d0f74a6b0b0afc5f927c1165576afb19a3d
Javier Segovia <javier.segovia@hoytrabajas.com>
2023-05-23 15:00:23 -0400

    Fix employee link

--- app/routes/dashboard/manage/employees.tsx
+++ app/routes/dashboard/manage/employees.tsx
@@ -94,7 +94,7 @@ export default function DashboardEmployeesIndexRoute() {
   const rows: TableRowProps[] = employees?.map(
     ({ id, user, city, jobDepartment, employeeGroups, benefits, status }) => ({
       rowId: id,
-      href: `${id}`,
+      href: `${id}/account`,
       items: [
         <>
           <span className="whitespace-pre-wrap" key={`${id}_name`}>
