a521a5c889d74ba666123a87b2149f303a3a8c8e
Danzoategui <daniel.anzoategui@hoytrabajas.com>
2023-05-25 13:57:16 -0500

    Fix BenefitCard in Carrousel

--- app/components/Cards/BenefitHighlightCard.tsx
+++ app/components/Cards/BenefitHighlightCard.tsx
@@ -18,27 +18,35 @@ export const BenefitHighlightCard = ({
   benefitHighlight,
 }: BenefitHighlightCardProps) => {
   return (
-    <Box className="grid h-full grid-cols-5 items-stretch gap-2 overflow-hidden rounded-lg xl:flex-row">
-      <img
-        src={benefitHighlight.image?.url}
-        className="col-span-3 w-full bg-white object-cover"
-        alt="Highlight"
-      />
-      <div className="col-span-2 min-w-[40%] p-4">
-        <Title as="h3">{benefitHighlight.title}</Title>
-        <p className="mt-2 whitespace-pre-wrap text-gray-700">
-          {benefitHighlight.description}
-        </p>
-        <div className="inline-block w-full lg:max-w-[14rem]">
-          <Button
-            href={benefitHighlight.buttonHref}
-            external={isValidURL(benefitHighlight.buttonHref)}
-            className="mt-5"
-          >
-            {benefitHighlight.buttonText}
-          </Button>
-        </div>
-      </div>
-    </Box>
+<Box className="grid h-full grid-cols-5 items-stretch gap-2 overflow-hidden rounded-lg xl:flex-row">
+  {/* Imagen */}
+  <img
+    src={benefitHighlight.image?.url}
+    className="col-span-5 md:col-span-3 w-full bg-white object-cover"
+    alt="Highlight"
+  />
+
+  <div className="col-span-5 md:col-span-2 p-4">
+    {/* Título */}
+    <Title as="h3">{benefitHighlight.title}</Title>
+
+    {/* Descripción */}
+    <p className="mt-2 whitespace-pre-wrap text-gray-700">
+      {benefitHighlight.description}
+    </p>
+
+    <div className="mt-5">
+      {/* Botón */}
+      <Button
+        href={benefitHighlight.buttonHref}
+        external={isValidURL(benefitHighlight.buttonHref)}
+        className="w-full"
+      >
+        {benefitHighlight.buttonText}
+      </Button>
+    </div>
+  </div>
+</Box>
+
   )
 }
